8fccc72998bb2e4e78decb57254334f8561032e2
david.brouste <david.brouste@deveo.nc>
2023-11-16 10:12:17 +1100

    fix(env): fix env

--- src/lib/swagger.ts
+++ src/lib/swagger.ts
@@ -8,15 +8,15 @@ export const getApiDocs = async (): Promise<any> => {
     const oldApiName = '/b2b/apigee/v1/boites-postales';
     fileContents = fileContents.replace(oldApiName, '/api/bp');
     const spec = YAML.parse(fileContents);
-    spec.servers[0].url = process.env.HOSTNAME;
+    let url = process.env.VERCEL_ENV === 'local' ? 'http://' : 'https://';
+    url += process.env.VERCEL_URL;
+    url += process.env.VERCEL_ENV === 'local' ? ':3000' : '';
+    spec.servers[0].url = url;
     spec.openapi = '3.1.0'
     spec.info.title = 'OpenAPI definition for Apigee Controller'
     spec.info.version = 'v1'
     spec.security = []
     spec.servers[0].description = 'Serveur de test'
-    if (process.env.PORT !== '80') {
-        spec.servers[0].url += ':' + process.env.PORT;
-    }
     return spec;
 };
 
